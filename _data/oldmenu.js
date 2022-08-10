6563702976980df60ae19d3c417fe50c7f644afb
Eric A. Meyer <eric@meyerweb.com>
2022-08-10 12:14:26 -0400

    Changing navbar menu build data

--- _data/oldmenu.js
+++
@@ -1,8 +0,0 @@
-module.exports = [
-	{ title: "Learn & Discover", url: "/about/" },
-	{ title: "Blog", url: "/blog/" },
-	// Revisiting, this seems redundant because we added download/code/learn and discover
-	// { title: "Developers", url: "/developers" },
-	// { title: "CODE", url: "/code" },
-	{ "title": "Get Started", "url": "#", "class": "btn cta" },
-];
